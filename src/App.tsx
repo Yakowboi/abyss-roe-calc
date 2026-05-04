979837e6cfc6a386942024e62d9ff496fce9753a
Yakowboi <ariannawas@gmail.com>
2026-05-05 00:02:28 +0200

    feat: add a temporary select with an option for every fish from fishData

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 // import { useState } from "react";
+import { fishData } from "./data/fish";
 
 function App() {
   // const [count, setCount] = useState(0);
@@ -6,6 +7,14 @@ function App() {
   return (
     <>
       <h1>Here's a nice heading</h1>
+      <select name="fish" id="fish-list">
+        <option value="none">None</option>
+        {fishData.data.map((fish) => (
+          <option key={fish.id} value={fish.id} className={fish.rarity}>
+            {fish.name}
+          </option>
+        ))}
+      </select>
     </>
   );
 }
